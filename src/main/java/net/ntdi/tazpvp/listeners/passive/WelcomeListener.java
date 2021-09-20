583ce1412ad6aaa5a85f2d8e1115086ddb929d80
FalseCodee <falsecodee@gmail.com>
2021-09-19 20:24:26 -0400

    Condense /starter

--- src/main/java/net/ntdi/tazpvp/listeners/passive/WelcomeListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/WelcomeListener.java
@@ -1,6 +1,7 @@
 package net.ntdi.tazpvp.listeners.passive;
 
 import net.ntdi.tazpvp.TazPvP;
+import net.ntdi.tazpvp.utils.PlayerUtils;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -34,63 +35,7 @@ public class WelcomeListener implements Listener {
 
             event.setJoinMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + "+" + ChatColor.GRAY + "] " + p.getName());
             Player player = event.getPlayer();
-            if (player.getInventory().getArmorContents() != null) {
-                ItemStack armor1 = new ItemStack(Material.LEATHER_BOOTS);
-                ItemStack armor2 = new ItemStack(Material.LEATHER_HELMET);
-                ItemStack armor3 = new ItemStack(Material.LEATHER_CHESTPLATE);
-                ItemStack armor4 = new ItemStack(Material.LEATHER_LEGGINGS);
-                ItemStack sword = new ItemStack(Material.WOOD_SWORD);
-                ItemStack pickaxe = new ItemStack(Material.WOOD_PICKAXE);
-                ItemStack fishingrod = new ItemStack(Material.FISHING_ROD);
-                ItemStack bow = new ItemStack(Material.BOW);
-                ItemStack steak = new ItemStack(Material.COOKED_BEEF, 10);
-                ItemStack arrow = new ItemStack(Material.ARROW, 15);
-
-                ItemMeta meta1 = armor1.getItemMeta();
-                meta1.spigot().setUnbreakable(true);
-                armor1.setItemMeta(meta1);
-
-                ItemMeta meta2 = armor2.getItemMeta();
-                meta2.spigot().setUnbreakable(true);
-                armor2.setItemMeta(meta2);
-
-                ItemMeta meta3 = armor3.getItemMeta();
-                meta3.spigot().setUnbreakable(true);
-                armor3.setItemMeta(meta3);
-
-                ItemMeta meta4 = armor4.getItemMeta();
-                meta4.spigot().setUnbreakable(true);
-                armor4.setItemMeta(meta4);
-
-                ItemMeta swordMeta = sword.getItemMeta();
-                swordMeta.spigot().setUnbreakable(true);
-                sword.setItemMeta(swordMeta);
-
-                ItemMeta pickaxeMeta = sword.getItemMeta();
-                pickaxeMeta.spigot().setUnbreakable(true);
-                pickaxe.setItemMeta(pickaxeMeta);
-
-                ItemMeta fishingMeta = fishingrod.getItemMeta();
-                fishingMeta.spigot().setUnbreakable(true);
-                fishingrod.setItemMeta(fishingMeta);
-
-                ItemMeta bowMeta = bow.getItemMeta();
-                bowMeta.spigot().setUnbreakable(true);
-                bow.setItemMeta(bowMeta);
-
-
-                PlayerInventory inv = player.getInventory();
-                inv.setHelmet(armor2);
-                inv.setChestplate(armor3);
-                inv.setLeggings(armor4);
-                inv.setBoots(armor1);
-                inv.addItem(sword);
-                inv.addItem(fishingrod);
-                inv.addItem(bow);
-                inv.addItem(pickaxe);
-                inv.addItem(steak);
-                inv.addItem(arrow);
-            }
+            PlayerUtils.equipStarter(player);
         }
     }
     @EventHandler
