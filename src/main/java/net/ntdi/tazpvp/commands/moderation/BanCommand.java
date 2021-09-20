d8f79a0d85d806a6aeed0d58a94c0d173664f710
FalseCodee <falsecodee@gmail.com>
2021-09-20 16:28:50 -0400

    Ban webhook + bug fixes

--- src/main/java/net/ntdi/tazpvp/commands/moderation/BanCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/moderation/BanCommand.java
@@ -2,15 +2,21 @@ package net.ntdi.tazpvp.commands.moderation;
 
 import net.ntdi.tazpvp.TazPvP;
 import net.ntdi.tazpvp.utils.StringUtils;
+import net.ntdi.tazpvp.utils.https.PostHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
 
 public class BanCommand implements CommandExecutor {
     @Override
+    @SuppressWarnings("unchecked")
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = null;
         if(sender instanceof Player) {
@@ -25,7 +31,7 @@ public class BanCommand implements CommandExecutor {
                 String reason = StringUtils.buildString(args, 1);
                 if(banned != null){
                     if(TazPvP.punishmentManager.isBanned(banned)){
-                        TazPvP.punishmentManager.removeBan(player);
+                        TazPvP.punishmentManager.removeBan(banned);
                         player.sendMessage(ChatColor.RED + banned.getName() + " has been unbanned.");
                         banned.sendMessage(ChatColor.RED+"You have been unbanned.");
 
@@ -34,7 +40,33 @@ public class BanCommand implements CommandExecutor {
                         TazPvP.punishmentManager.initBan(banned, false, 5*1000);
                         player.sendMessage(ChatColor.RED + banned.getName() + " has been banned.");
                         banned.sendMessage(ChatColor.RED+"You have been banned for "+ChatColor.WHITE+reason);
+                        JSONObject obj = new JSONObject();
+                        JSONArray embed = new JSONArray();
+                        JSONObject embedObj = new JSONObject();
+                        embed.add(embedObj);
 
+                        embedObj.put("title", banned.getName());
+                        embedObj.put("color", 16077890);
+
+                        JSONArray fields = new JSONArray();
+                        JSONObject fieldBanned = new JSONObject();
+                        fieldBanned.put("name", "Ban Info");
+                        fieldBanned.put("value", "**Banned Account:** " + banned.getName()+"\n**Reason:** " + reason);
+                        JSONObject fieldOther = new JSONObject();
+                        fieldOther.put("name", "Other Info");
+                        fieldOther.put("value", "**Banner:** " + player.getName()+"\n**Time:** " + StringUtils.dateToString(System.currentTimeMillis()));
+                        fields.add(fieldBanned);
+                        fields.add(fieldOther);
+                        embedObj.put("fields", fields);
+                        obj.put("embeds", embed);
+                        obj.put("username", "Banner");
+                        obj.put("avatar_url", "");
+                        try {
+                            PostHelper.postRequest(TazPvP.configFile.getString("webhooks.bans"), obj.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            player.sendMessage(ChatColor.RED + "Webhook failed, please contact an admin.");
+                        }
                     }
                 } else {
                     player.sendMessage(ChatColor.RED + "Player not found.");
