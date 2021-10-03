615330f29c39b39d52ca70b94041a062a1c099a9
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-03 13:26:35 -0400

    only swords and picakxe give exp on hit

--- src/main/java/net/ntdi/tazpvp/listeners/function/EntityDamageByEntityListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/function/EntityDamageByEntityListener.java
@@ -1,10 +1,15 @@
 package net.ntdi.tazpvp.listeners.function;
 
 import net.ntdi.tazpvp.TazPvP;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class EntityDamageByEntityListener implements Listener {
 
@@ -14,7 +19,22 @@ public class EntityDamageByEntityListener implements Listener {
             if(!event.getDamager().getLocation().getWorld().getName().equals(TazPvP.configFile.getString("arena.name"))){
                 return;
             }
-            ((Player) event.getDamager()).giveExp(1);
+
+            ItemStack air = new ItemStack(Material.AIR);
+            ArrayList<Material> swords = new ArrayList<>();
+            swords.add(Material.WOOD_SWORD);
+            swords.add(Material.STONE_SWORD);
+            swords.add(Material.IRON_SWORD);
+            swords.add(Material.DIAMOND_SWORD);
+            swords.add(Material.WOOD_PICKAXE);
+            swords.add(Material.STONE_PICKAXE);
+            swords.add(Material.IRON_PICKAXE);
+            swords.add(Material.DIAMOND_PICKAXE);
+
+
+            if(swords.contains(((Player) event.getDamager()).getItemInHand())){
+                ((Player) event.getDamager()).giveExp(1);
+            }
         }
     }
 }
