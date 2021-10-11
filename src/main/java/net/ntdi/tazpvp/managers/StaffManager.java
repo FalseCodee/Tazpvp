0655dae324f764d90840d5c0da87f1c73a031177
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-10 21:36:28 -0400

    Changed sound to be higher

--- src/main/java/net/ntdi/tazpvp/managers/StaffManager.java
+++ src/main/java/net/ntdi/tazpvp/managers/StaffManager.java
@@ -43,7 +43,7 @@ public class StaffManager {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (p.hasPermission("staff.staffchat")){
                 if (p != player){
-                    p.playSound(p.getLocation(), Sound.NOTE_PIANO, 3, 1);
+                    p.playSound(p.getLocation(), Sound.NOTE_PIANO, 1, 1);
                 }
             }
         }
