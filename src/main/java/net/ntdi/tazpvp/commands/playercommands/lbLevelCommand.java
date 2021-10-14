f55d02141327abe73eee5f239fa502e965d132b8
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-14 16:32:16 -0400

    lb sorter?

--- src/main/java/net/ntdi/tazpvp/commands/playercommands/lbLevelCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/playercommands/lbLevelCommand.java
@@ -1,5 +1,6 @@
 package net.ntdi.tazpvp.commands.playercommands;
 
+import net.bytebuddy.matcher.HasSuperClassMatcher;
 import net.ntdi.tazpvp.TazPvP;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -9,7 +10,15 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 public class lbLevelCommand implements CommandExecutor {
+
+    private static HashMap<OfflinePlayer, Integer> hashMap = new HashMap<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -17,8 +26,12 @@ public class lbLevelCommand implements CommandExecutor {
 
         p.sendMessage(ChatColor.GREEN + "----------");
         for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
-
-            p.sendMessage(TazPvP.statsManager.getLevel(player) + "\n");
+            hashMap.put(player, TazPvP.statsManager.getLevel(player));
+        }
+        List list = new ArrayList(hashMap.values());
+        Collections.sort(list);
+        for(int i=0; i<10; i++) {
+            p.sendMessage(list.toString() + "\n");
         }
 
         return true;
