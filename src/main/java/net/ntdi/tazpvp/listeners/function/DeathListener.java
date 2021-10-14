2e13095cbe262b22a0135c31e9457b34f16d6306
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-14 15:32:43 -0400

    regen

--- src/main/java/net/ntdi/tazpvp/listeners/function/DeathListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/function/DeathListener.java
@@ -45,11 +45,14 @@ public class DeathListener implements Listener {
                     killer.sendMessage(ChatColor.DARK_GRAY + "You killed " + ChatColor.GRAY + "" + p.getName() + ChatColor.GOLD + " + 5 Coins " + ChatColor.DARK_AQUA + "+ 8 Experience");
                     killer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 1, true, false));
                     killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 40, 1, true, false));
-                    killer.setHealth(killer.getHealth() + 8);
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 5, 3, true, false));
                 } else {
                     killer.giveExp(5);
                     killer.sendMessage(ChatColor.DARK_GRAY + "You killed " + ChatColor.GRAY + "" + p.getName() + ChatColor.GOLD + " + 5 Coins " + ChatColor.DARK_AQUA + "+ 5 Experience");
                     killer.setHealth(Math.min(killer.getHealth() + 6, 20));
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 40, 1, true, false));
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 40, 1, true, false));
+                    killer.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 5, 3, true, false));
                 }
                 p.sendMessage(ChatColor.DARK_GRAY + "You were killed by " + ChatColor.GRAY + "" + killer.getName());
             }
