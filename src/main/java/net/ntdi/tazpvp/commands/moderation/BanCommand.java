fb586294ad53dc748013d0acbbd6d279f2a1ac25
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-15 15:52:51 -0400

    checked if banned != null

--- src/main/java/net/ntdi/tazpvp/commands/moderation/BanCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/moderation/BanCommand.java
@@ -36,113 +36,116 @@ public class BanCommand implements CommandExecutor {
             return true;
         }
         Player banned = Bukkit.getPlayer(args[0]);
-        if(player != null && player.hasPermission("staff.ban") && !banned.hasPermission("staff.banbypass")){
-            if(args.length < 2){
-                return false;
-            } else {
-                String reason = StringUtils.buildString(args, 1);
-                if(banned != null){
-                    if(TazPvP.punishmentManager.isBanned(banned)){
-                        TazPvP.punishmentManager.removeBan(banned);
-                        player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN " + ChatColor.RED + "You just unbanned " + ChatColor.WHITE + banned.getName());
-                        banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                        banned.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBANNED" + ChatColor.GRAY + " You've been unbanned by " + ChatColor.WHITE + player.getName());
-                        banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                        if(bannedRunnables.containsKey(banned.getUniqueId())) {
-                            bannedRunnables.get(banned.getUniqueId()).cancel();
-                            bannedRunnables.remove(banned.getUniqueId());
-                        }
-
-                        for(Scoreboard sb : TazPvP.statsManager.scoreboards.values()) {
-                            TazPvP.statsManager.getTeam(banned, sb).removeEntry(banned.getName());
-                        }
-
-                        TazPvP.statsManager.scoreboards.remove(banned.getUniqueId());
-
-                        new BukkitRunnable() {
-
-                            @Override
-                            public void run() {
-                                TazPvP.statsManager.initScoreboard(banned);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 20L);
-
-                    } else {
-                        //short ban to test
-                        TazPvP.punishmentManager.initBan(banned, false, 60*60*1000);
-                        player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BAN " + ChatColor.RED + "You just banned " + ChatColor.WHITE + banned.getName());
-                        banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                        banned.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BANNED" + ChatColor.GRAY + " You've been banned for "+ChatColor.WHITE+reason + ChatColor.GRAY + " by " + ChatColor.WHITE + player.getName());
-                        banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-
-
-                        for(Scoreboard sb : TazPvP.statsManager.scoreboards.values()) {
-                            TazPvP.statsManager.getTeam(banned, sb).removeEntry(banned.getName());
-                        }
-
-                        TazPvP.statsManager.scoreboards.remove(banned.getUniqueId());
-
-                        new BukkitRunnable() {
-
-                            @Override
-                            public void run() {
-                                TazPvP.statsManager.initScoreboard(banned);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 20L);
-
-                        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-                        String pexcmd = "/pex user " + banned.getName() + " group add banned";
-                        Bukkit.dispatchCommand(console, pexcmd);
-
-                        for (Player p : Bukkit.getOnlinePlayers()){
-                            if (!Objects.equals(p.getName(), banned.getName())){
-                                p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                                p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BANNED " + ChatColor.WHITE + banned.getName() + ChatColor.GRAY + " has been punished for " + ChatColor.WHITE + reason);
-                                p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                            }
-                        }
-
-
-
-                        BukkitRunnable runnable = getBanRunnable(banned);
-                        runnable.runTaskTimer(TazPvP.getInstance(), 0L, 1200L);
-                        bannedRunnables.put(banned.getUniqueId(), runnable);
-
-                        JSONObject obj = new JSONObject();
-                        JSONArray embed = new JSONArray();
-                        JSONObject embedObj = new JSONObject();
-                        embed.add(embedObj);
-
-                        embedObj.put("title", banned.getName());
-                        embedObj.put("color", 16077890);
-
-                        JSONArray fields = new JSONArray();
-                        JSONObject fieldBanned = new JSONObject();
-                        fieldBanned.put("name", "Ban Info");
-                        fieldBanned.put("value", "**Banned Account:** " + banned.getName()+"\n**Reason:** " + reason);
-                        JSONObject fieldOther = new JSONObject();
-                        fieldOther.put("name", "Other Info");
-                        fieldOther.put("value", "**Banner:** " + player.getName()+"\n**Time:** " + StringUtils.dateToString(System.currentTimeMillis()));
-                        fields.add(fieldBanned);
-                        fields.add(fieldOther);
-                        embedObj.put("fields", fields);
-                        obj.put("embeds", embed);
-                        obj.put("username", "Banner");
-                        obj.put("avatar_url", "");
-                        try {
-                            PostHelper.postRequest(TazPvP.configFile.getString("webhooks.bans"), obj.toString());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            player.sendMessage(ChatColor.RED + "Webhook failed, please contact an admin.");
-                        }
-                    }
+        if (banned != null) {
+            if(player != null && player.hasPermission("staff.ban") && !banned.hasPermission("staff.banbypass")){
+                if(args.length < 2){
+                    return false;
                 } else {
-                    player.sendMessage(ChatColor.RED + "Player not found.");
+                    String reason = StringUtils.buildString(args, 1);
+                    if(banned != null){
+                        if(TazPvP.punishmentManager.isBanned(banned)){
+                            TazPvP.punishmentManager.removeBan(banned);
+                            player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN " + ChatColor.RED + "You just unbanned " + ChatColor.WHITE + banned.getName());
+                            banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                            banned.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBANNED" + ChatColor.GRAY + " You've been unbanned by " + ChatColor.WHITE + player.getName());
+                            banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                            if(bannedRunnables.containsKey(banned.getUniqueId())) {
+                                bannedRunnables.get(banned.getUniqueId()).cancel();
+                                bannedRunnables.remove(banned.getUniqueId());
+                            }
+
+                            for(Scoreboard sb : TazPvP.statsManager.scoreboards.values()) {
+                                TazPvP.statsManager.getTeam(banned, sb).removeEntry(banned.getName());
+                            }
+
+                            TazPvP.statsManager.scoreboards.remove(banned.getUniqueId());
+
+                            new BukkitRunnable() {
+
+                                @Override
+                                public void run() {
+                                    TazPvP.statsManager.initScoreboard(banned);
+                                }
+                            }.runTaskLater(TazPvP.getInstance(), 20L);
+
+                        } else {
+                            //short ban to test
+                            TazPvP.punishmentManager.initBan(banned, false, 60*60*1000);
+                            player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BAN " + ChatColor.RED + "You just banned " + ChatColor.WHITE + banned.getName());
+                            banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                            banned.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BANNED" + ChatColor.GRAY + " You've been banned for "+ChatColor.WHITE+reason + ChatColor.GRAY + " by " + ChatColor.WHITE + player.getName());
+                            banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+
+                            for(Scoreboard sb : TazPvP.statsManager.scoreboards.values()) {
+                                TazPvP.statsManager.getTeam(banned, sb).removeEntry(banned.getName());
+                            }
+
+                            TazPvP.statsManager.scoreboards.remove(banned.getUniqueId());
+
+                            new BukkitRunnable() {
+
+                                @Override
+                                public void run() {
+                                    TazPvP.statsManager.initScoreboard(banned);
+                                }
+                            }.runTaskLater(TazPvP.getInstance(), 20L);
+
+                            ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+                            String pexcmd = "/pex user " + banned.getName() + " group add banned";
+                            Bukkit.dispatchCommand(console, pexcmd);
+
+                            for (Player p : Bukkit.getOnlinePlayers()){
+                                if (!Objects.equals(p.getName(), banned.getName())){
+                                    p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                                    p.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "BANNED " + ChatColor.WHITE + banned.getName() + ChatColor.GRAY + " has been punished for " + ChatColor.WHITE + reason);
+                                    p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                                }
+                            }
+
+
+
+                            BukkitRunnable runnable = getBanRunnable(banned);
+                            runnable.runTaskTimer(TazPvP.getInstance(), 0L, 1200L);
+                            bannedRunnables.put(banned.getUniqueId(), runnable);
+
+                            JSONObject obj = new JSONObject();
+                            JSONArray embed = new JSONArray();
+                            JSONObject embedObj = new JSONObject();
+                            embed.add(embedObj);
+
+                            embedObj.put("title", banned.getName());
+                            embedObj.put("color", 16077890);
+
+                            JSONArray fields = new JSONArray();
+                            JSONObject fieldBanned = new JSONObject();
+                            fieldBanned.put("name", "Ban Info");
+                            fieldBanned.put("value", "**Banned Account:** " + banned.getName()+"\n**Reason:** " + reason);
+                            JSONObject fieldOther = new JSONObject();
+                            fieldOther.put("name", "Other Info");
+                            fieldOther.put("value", "**Banner:** " + player.getName()+"\n**Time:** " + StringUtils.dateToString(System.currentTimeMillis()));
+                            fields.add(fieldBanned);
+                            fields.add(fieldOther);
+                            embedObj.put("fields", fields);
+                            obj.put("embeds", embed);
+                            obj.put("username", "Banner");
+                            obj.put("avatar_url", "");
+                            try {
+                                PostHelper.postRequest(TazPvP.configFile.getString("webhooks.bans"), obj.toString());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                player.sendMessage(ChatColor.RED + "Webhook failed, please contact an admin.");
+                            }
+                        }
+                    } else {
+                        player.sendMessage(ChatColor.RED + "Player not found.");
+                    }
                 }
+            } else {
+                player.sendMessage(ChatColor.RED + "You cant ban this person!");
             }
-        } else {
-            player.sendMessage(ChatColor.RED + "You cant ban this person!");
         }
+
 
         return true;
     }
