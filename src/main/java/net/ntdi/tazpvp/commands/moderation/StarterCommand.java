02c6e5c756b6b757e5bca18b70b3994c9a817e6e
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-16 21:27:54 -0400

    starter stqarrters u even more

--- src/main/java/net/ntdi/tazpvp/commands/moderation/StarterCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/moderation/StarterCommand.java
@@ -1,5 +1,6 @@
 package net.ntdi.tazpvp.commands.moderation;
 
+import net.ntdi.tazpvp.TazPvP;
 import net.ntdi.tazpvp.utils.PlayerUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -19,6 +20,12 @@ public class StarterCommand implements CommandExecutor {
                     Player target = Bukkit.getPlayer(args[0]);
                 if(target != null){
                     PlayerUtils.equipStarter(target);
+                    TazPvP.statsManager.setMoney(target, 0);
+                    TazPvP.statsManager.setLevel(target, 0);
+                    TazPvP.statsManager.setPoints(target, 0);
+                    TazPvP.statsManager.setCredits(target, 0);
+                    target.setExp(0L);
+
                 } else {
                     p.sendMessage(ChatColor.RED + "Player not found.");
                 }
