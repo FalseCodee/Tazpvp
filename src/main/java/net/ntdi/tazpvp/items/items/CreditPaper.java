db3f08698de633e659052fddd56d07c8634e2af7
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-18 16:52:17 -0400

    credit paper uwu

---
+++ src/main/java/net/ntdi/tazpvp/items/items/CreditPaper.java
@@ -0,0 +1,29 @@
+package net.ntdi.tazpvp.items.items;
+
+import net.ntdi.tazpvp.TazPvP;
+import net.ntdi.tazpvp.items.ConsumableItem;
+import net.ntdi.tazpvp.items.Items;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class CreditPaper extends ConsumableItem {
+
+    public CreditPaper() {
+        super(Items.CREDITPAPER, 0);
+    }
+
+    @Override
+    public boolean execute(Player p, ItemStack itemStack) {
+        if(super.execute(p, itemStack)) {
+            return true;
+        }
+
+        p.sendMessage(ChatColor.RED + "YOOOOOOOOOOOOOOOOOO CLAIMNIN 50 CWEDITS!!!!!!!!! SO COOOOOOOOOOOLLLLLLLLLLLLLLLLLL");
+        TazPvP.statsManager.addCredits(p, 50);
+
+        return false;
+    }
+}
