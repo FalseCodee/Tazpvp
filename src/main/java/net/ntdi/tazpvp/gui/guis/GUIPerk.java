535ddf1cd7ccf5ac1ddbed37c63dd39422a247cf
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-10-23 19:39:03 -0400

    hiding item flags

--- src/main/java/net/ntdi/tazpvp/gui/guis/GUIPerk.java
+++ src/main/java/net/ntdi/tazpvp/gui/guis/GUIPerk.java
@@ -8,7 +8,9 @@ import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class GUIPerk extends GUI {
     public GUIPerk(Player player) {
@@ -151,13 +153,19 @@ public class GUIPerk extends GUI {
         for(int i = 0; i < inventory.getSize(); i++) {
             items[i] = createItem(new ItemStack(Material.STAINED_GLASS_PANE, 1, DyeColor.BLACK.getData()), ChatColor.BLACK + "");
         }
+
+        ItemStack pickaxe = new ItemStack(Material.GOLD_PICKAXE, 1);
+        ItemMeta pickaxeMeta = pickaxe.getItemMeta();
+        pickaxeMeta.removeItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        pickaxe.setItemMeta(pickaxeMeta);
+
         addShopItem(10, new ItemStack(Material.WOOD, 1), 1, ChatColor.DARK_AQUA + "BUY SAVE BLOCKS PERK", ChatColor.GRAY + "Get a chance to get not loose your blocks when you place it\n" + ChatColor.YELLOW + "$500");
         addShopItem(11, new ItemStack(Material.GOLD_INGOT, 1), 2, ChatColor.DARK_AQUA + "BUY BUTTER PERK", ChatColor.GRAY + "Get a chance to get butter affects on kill\n" + ChatColor.YELLOW + "$500");
         addShopItem(12, new ItemStack(Material.EYE_OF_ENDER, 1), 3, ChatColor.DARK_AQUA + "BUY AGILITY PERK", ChatColor.GRAY + "Get a chance to get agility affects on kill\n" + ChatColor.YELLOW + "$500");
         addShopItem(13, new ItemStack(Material.RED_ROSE, 1), 4, ChatColor.DARK_AQUA + "BUY EXTINGUISH PERK", ChatColor.GRAY + "Get a chance to get extinguish yourself on hit\n" + ChatColor.YELLOW + "$250");
         addShopItem(14, new ItemStack(Material.FEATHER, 1), 5, ChatColor.DARK_AQUA + "BUY FALLDAMAGE PERK", ChatColor.GRAY + "Get a chance to get ignore fall damage when you fall\n" + ChatColor.YELLOW + "$500");
         addShopItem(15, new ItemStack(Material.COOKED_BEEF, 1), 6, ChatColor.DARK_AQUA + "BUY HUNGER PERK", ChatColor.GRAY + "Get a chance to get refuel your hunger bar on kill\n" + ChatColor.YELLOW + "$500");
-        addShopItem(16, new ItemStack(Material.GOLD_PICKAXE, 1), 7, ChatColor.DARK_AQUA + "BUY HASTE PERK", ChatColor.GRAY + "Get a chance to get haste affects on ore mined\n" + ChatColor.YELLOW + "$500");
+        addShopItem(16, pickaxe, 7, ChatColor.DARK_AQUA + "BUY HASTE PERK", ChatColor.GRAY + "Get a chance to get haste affects on ore mined\n" + ChatColor.YELLOW + "$500");
         addShopItem(19, new ItemStack(Material.IRON_FENCE, 1), 8, ChatColor.DARK_AQUA + "BUY ROBBERY PERK", ChatColor.GRAY + "Get a chance to disarm your opponent whilst hitting them without holding your sword\n" + ChatColor.YELLOW + "$500");
         addShopItem(20, new ItemStack(Material.POTION, 1), 9, ChatColor.DARK_AQUA + "BUY STRENGTH PERK", ChatColor.GRAY + "Get a chance to get strength affects on kill\n" + ChatColor.YELLOW + "$1000");
 
