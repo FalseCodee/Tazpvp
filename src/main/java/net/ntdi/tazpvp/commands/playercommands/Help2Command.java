d7189f31884e215c6d5e2afc2c95b847e1e51a96
rownox <rownox.ik@gmail.com>
2021-10-30 09:46:50 -0400

    Fixed /help2

--- src/main/java/net/ntdi/tazpvp/commands/playercommands/Help2Command.java
+++ src/main/java/net/ntdi/tazpvp/commands/playercommands/Help2Command.java
@@ -17,14 +17,14 @@ public class Help2Command implements CommandExecutor {
         if(sender instanceof Player){
             Player p = (Player) sender;
             p.sendMessage(ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----" + ChatColor.RED + "TAZPVP" + ChatColor.AQUA + "-----" + ChatColor.DARK_AQUA + "-----" + ChatColor.AQUA + "-----");
-            p.sendMessage(ChatColor.WHITE + "/report" + ChatColor.GRAY + "Report rule breakers.");
-            p.sendMessage(ChatColor.WHITE + "/apply" + ChatColor.GRAY + "Apply for a staff position.");
-            p.sendMessage(ChatColor.WHITE + "/spawn" + ChatColor.GRAY + "Teleport back to the spawn.");
-            p.sendMessage(ChatColor.WHITE + "/discord" + ChatColor.GRAY + "Join the community.");
-            p.sendMessage(ChatColor.WHITE + "/report" + ChatColor.GRAY + "Report rule breakers.");
-            p.sendMessage(ChatColor.WHITE + "/duel" + ChatColor.GRAY + "Battle versus another player.");
+            p.sendMessage(ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.");
+            p.sendMessage(ChatColor.WHITE + "/apply " + ChatColor.GRAY + "Apply for a staff position.");
+            p.sendMessage(ChatColor.WHITE + "/spawn " + ChatColor.GRAY + "Teleport back to the spawn.");
+            p.sendMessage(ChatColor.WHITE + "/discord " + ChatColor.GRAY + "Join the community.");
+            p.sendMessage(ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.");
+            p.sendMessage(ChatColor.WHITE + "/duel " + ChatColor.GRAY + "Battle versus another player.");
             p.sendMessage(ChatColor.GRAY + "");
-            p.sendMessage(ChatColor.GREEN + "PAGE [2/2]" + ChatColor.GRAY + "/help to go back");
+            p.sendMessage(ChatColor.GREEN + "PAGE [2/2] " + ChatColor.GRAY + "/help to go back");
         }else{
             System.out.println("Only players can execute /help");
         }
