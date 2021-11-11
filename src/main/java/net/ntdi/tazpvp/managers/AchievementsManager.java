f37c9cad244d00ae070de98c52aa31a6fdfe0945
rownox <rownox.ik@gmail.com>
2021-11-11 16:54:35 -0500

    Nerfed achievement rewards

--- src/main/java/net/ntdi/tazpvp/managers/AchievementsManager.java
+++ src/main/java/net/ntdi/tazpvp/managers/AchievementsManager.java
@@ -28,21 +28,21 @@ public class AchievementsManager {
     public void init() {
         //achievements.add(new DeathAchievement(5, new Rewards[] {Rewards.MONEY}, new int[] {100}));
         achievements.add(new DeathAchievement(50, new Rewards[] {Rewards.MONEY}, new int[] {100}));
-        achievements.add(new DeathAchievement(100, new Rewards[] {Rewards.POINTS}, new int[] {3}));
-        achievements.add(new DeathAchievement(200, new Rewards[] {Rewards.MONEY}, new int[] {250}));
-        achievements.add(new DeathAchievement(300, new Rewards[] {Rewards.POINTS}, new int[] {10}));
-        achievements.add(new DeathAchievement(400, new Rewards[] {Rewards.MONEY}, new int[] {2500}));
-        achievements.add(new DeathAchievement(500, new Rewards[] {Rewards.POINTS}, new int[] {15}));
-        achievements.add(new DeathAchievement(1000, new Rewards[] {Rewards.MONEY}, new int[] {10000}));
+        achievements.add(new DeathAchievement(100, new Rewards[] {Rewards.POINTS}, new int[] {1}));
+        achievements.add(new DeathAchievement(200, new Rewards[] {Rewards.MONEY}, new int[] {300}));
+        achievements.add(new DeathAchievement(300, new Rewards[] {Rewards.POINTS}, new int[] {3}));
+        achievements.add(new DeathAchievement(400, new Rewards[] {Rewards.MONEY}, new int[] {1000}));
+        achievements.add(new DeathAchievement(500, new Rewards[] {Rewards.POINTS}, new int[] {5}));
+        achievements.add(new DeathAchievement(1000, new Rewards[] {Rewards.MONEY}, new int[] {2000}));
 
         //achievements.add(new KillAchievement(5, new Rewards[] {Rewards.MONEY}, new int[] {200}));
-        achievements.add(new KillAchievement(25, new Rewards[] {Rewards.MONEY}, new int[] {200}));
-        achievements.add(new KillAchievement(50, new Rewards[] {Rewards.POINTS}, new int[] {5}));
+        achievements.add(new KillAchievement(25, new Rewards[] {Rewards.MONEY}, new int[] {100}));
+        achievements.add(new KillAchievement(50, new Rewards[] {Rewards.POINTS}, new int[] {2}));
         achievements.add(new KillAchievement(100, new Rewards[] {Rewards.MONEY}, new int[] {500}));
-        achievements.add(new KillAchievement(200, new Rewards[] {Rewards.POINTS}, new int[] {15}));
+        achievements.add(new KillAchievement(200, new Rewards[] {Rewards.POINTS}, new int[] {4}));
         achievements.add(new KillAchievement(500, new Rewards[] {Rewards.MONEY}, new int[] {1000}));
-        achievements.add(new KillAchievement(1000, new Rewards[] {Rewards.POINTS}, new int[] {25}));
-        achievements.add(new KillAchievement(1500, new Rewards[] {Rewards.MONEY}, new int[] {15000}));
+        achievements.add(new KillAchievement(1000, new Rewards[] {Rewards.POINTS}, new int[] {8}));
+        achievements.add(new KillAchievement(1500, new Rewards[] {Rewards.MONEY}, new int[] {3000}));
 
         //kill streak achievement
 
@@ -52,19 +52,19 @@ public class AchievementsManager {
         achievements.add(new LivingOnEdgeAchievement(1, new Rewards[] {Rewards.MONEY}, new int[] {50}));
 
         // tis but a scratch
-        achievements.add(new ButAScratchAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {2}));
+        achievements.add(new ButAScratchAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {1}));
 
         // slowpoke
         achievements.add(new SlowPokeAchievement(1, new Rewards[] {Rewards.MONEY}, new int[] {50}));
 
         // takeover
-        achievements.add(new TakeOverAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {5}));
+        achievements.add(new TakeOverAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {3}));
 
         // Smart Player
         achievements.add(new SmartPlayerAchievement(1, new Rewards[] {Rewards.MONEY}, new int[] {100}));
 
         // Starting beef
-        achievements.add(new BeefAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {3}));
+        achievements.add(new BeefAchievement(1, new Rewards[] {Rewards.POINTS}, new int[] {1}));
 
         // Caught a fish
         achievements.add(new CaughtFishAchievement(1, new Rewards[] {Rewards.MONEY}, new int[] {50}));
