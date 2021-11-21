ba6ac7154d1b60951ca80dbcd172f2b38e410268
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-11-20 20:30:09 -0500

    fireball :D

---
+++ src/main/java/net/ntdi/tazpvp/items/items/Fireballz.java
@@ -0,0 +1,28 @@
+package net.ntdi.tazpvp.items.items;
+
+import net.ntdi.tazpvp.items.ClickableItem;
+import net.ntdi.tazpvp.items.ConsumableItem;
+import net.ntdi.tazpvp.items.Items;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Fireballz extends ConsumableItem {
+    public Fireballz() {
+        super(Items.FIREBALL, 1);
+    }
+
+    @Override
+    public boolean execute(Player p, ItemStack itemStack) {
+        if(super.execute(p, itemStack)) {
+            return true;
+        }
+
+        org.bukkit.entity.Fireball fireball = p.launchProjectile(org.bukkit.entity.Fireball.class);
+        fireball.setCustomName(ChatColor.RED + "Arabic person");
+        fireball.setCustomNameVisible(true);
+        fireball.setBounce(true);
+
+        return true;
+    }
+}
