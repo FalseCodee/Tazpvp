ab90c757eb9b7aea8247035e5dbf54348bb26a72
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-11-22 22:17:06 -0500

    fixing attempt 2

--- src/main/java/net/ntdi/tazpvp/TazPvP.java
+++ src/main/java/net/ntdi/tazpvp/TazPvP.java
@@ -251,7 +251,7 @@ public final class TazPvP extends JavaPlugin {
         getCommand("dailyrewards").setExecutor(new RedeemCommand());
         getCommand("achievement").setExecutor(new AchievementGuiCommand());
         getCommand("shop").setExecutor(new ShopGuiCommand());
-        getCommand("hologram").setExecutor(new HologramsCommand());
+        getCommand("hologramz").setExecutor(new HologramsCommand());
         getCommand("tazload").setExecutor(new RestartCommand());
         getCommand("ridepearl").setExecutor(new ridepearlCommand());
         getCommand("firegun").setExecutor(new fireballCommand());
