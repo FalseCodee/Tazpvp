3a207ebd19261e772e8e246176a853b97c1436ec
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2021-11-22 21:50:27 -0500

    no more armor take off >:(

--- src/main/java/net/ntdi/tazpvp/listeners/passive/ArmorTakeOffListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/ArmorTakeOffListener.java
@@ -1,5 +1,6 @@
 package net.ntdi.tazpvp.listeners.passive;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -9,10 +10,12 @@ public class ArmorTakeOffListener implements Listener {
     @EventHandler
     public void onArmorTakeOff(InventoryClickEvent event) {
         if (!event.getWhoClicked().hasPermission("staff.armor")) {
-            if (event.getClick().isLeftClick() || event.getClick().isRightClick() || event.getClick().isShiftClick() || event.getClick().isKeyboardClick()) {
-                if (event.getSlot() == 100 || event.getSlot() == 101 || event.getSlot() == 102 || event.getSlot() == 103) {
-                    event.setCancelled(true);
-                }
+            if (event.getCurrentItem().getType() == Material.LEATHER_BOOTS || event.getCurrentItem().getType() == Material.LEATHER_CHESTPLATE || event.getCurrentItem().getType() == Material.LEATHER_HELMET || event.getCurrentItem().getType() == Material.LEATHER_LEGGINGS
+                    || event.getCurrentItem().getType() == Material.CHAINMAIL_BOOTS || event.getCurrentItem().getType() == Material.CHAINMAIL_CHESTPLATE || event.getCurrentItem().getType() == Material.CHAINMAIL_HELMET || event.getCurrentItem().getType() == Material.CHAINMAIL_LEGGINGS
+                    || event.getCurrentItem().getType() == Material.IRON_BOOTS || event.getCurrentItem().getType() == Material.IRON_CHESTPLATE || event.getCurrentItem().getType() == Material.IRON_HELMET || event.getCurrentItem().getType() == Material.IRON_LEGGINGS
+                    || event.getCurrentItem().getType() == Material.DIAMOND_BOOTS || event.getCurrentItem().getType() == Material.DIAMOND_CHESTPLATE || event.getCurrentItem().getType() == Material.DIAMOND_HELMET || event.getCurrentItem().getType() == Material.DIAMOND_LEGGINGS
+                    || event.getCurrentItem().getType() == Material.GOLD_BOOTS || event.getCurrentItem().getType() == Material.GOLD_CHESTPLATE || event.getCurrentItem().getType() == Material.GOLD_HELMET || event.getCurrentItem().getType() == Material.GOLD_LEGGINGS) {
+                event.setCancelled(true);
             }
         }
     }
