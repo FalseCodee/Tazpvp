d355c95ab8e27f9391fb5f81e7e15ec92f4f65af
rownox <rownox.ik@gmail.com>
2022-01-04 20:08:25 -0500

    Fixed block respawn times

--- src/main/java/net/ntdi/tazpvp/listeners/function/BlockBreakListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/function/BlockBreakListener.java
@@ -1,6 +1,5 @@
 package net.ntdi.tazpvp.listeners.function;
 
-import jdk.tools.jlink.plugin.Plugin;
 import net.ntdi.tazpvp.TazPvP;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -11,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Coal;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -31,107 +31,47 @@ public class BlockBreakListener implements Listener {
             event.setCancelled(true);
             Block b = event.getBlock();
             Material mat = b.getType();
-
+            b.setType(Material.BEDROCK);
            if (TazPvP.blocks.contains(mat)) {
                 if (p.getItemInHand().hasItemMeta() && p.getItemInHand().getItemMeta().hasEnchant(Enchantment.SILK_TOUCH) && p.getItemInHand().getItemMeta().hasEnchant(Enchantment.LOOT_BONUS_BLOCKS)) {
                     if (mat == Material.COAL_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.COAL, 2));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.COAL_ORE, 120L, b);
                     } else if (mat == Material.IRON_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 2));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.IRON_ORE, 180L, b);
                     } else if (mat == Material.LAPIS_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.INK_SACK, 2, (short) 4));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.LAPIS_ORE, 260L, b);
                     } else if (mat == Material.GOLD_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.GOLD_INGOT, 2));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.GOLD_ORE, 350L, b);
                     } else if (mat == Material.DIAMOND_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.DIAMOND, 2));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.DIAMOND_ORE, 400L, b);
                     } else if (mat == Material.EMERALD_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.EMERALD, 2));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.EMERALD_ORE, 450L, b);
                     }
                 } else if (p.getItemInHand().hasItemMeta() && p.getItemInHand().getItemMeta().hasEnchant(Enchantment.SILK_TOUCH)){
                     if (mat == Material.COAL_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.COAL_ORE, 1));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.COAL_ORE, 120L, b);
                     } else if (mat == Material.IRON_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.IRON_INGOT, 1));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.IRON_ORE, 180L, b);
                     } else if (mat == Material.LAPIS_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.INK_SACK, 1, (short) 4));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.LAPIS_ORE, 260L, b);
                     } else if (mat == Material.GOLD_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.GOLD_INGOT, 1));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.GOLD_ORE, 350L, b);
                     } else if (mat == Material.DIAMOND_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.DIAMOND, 1));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.DIAMOND_ORE, 400L, b);
                     } else if (mat == Material.EMERALD_ORE) {
                         p.getInventory().addItem(new ItemStack(Material.EMERALD, 1));
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                b.setType(mat);
-                            }
-                        }.runTaskLater(TazPvP.getInstance(), 200L);
+                        respawnOre(Material.EMERALD_ORE, 450L, b);
                     }
                 } else if (p.getItemInHand().hasItemMeta() && p.getItemInHand().getItemMeta().hasEnchant(Enchantment.LOOT_BONUS_BLOCKS)) {
                     p.getInventory().addItem(new ItemStack(mat, 2));
@@ -142,14 +82,16 @@ public class BlockBreakListener implements Listener {
                 if (TazPvP.perkManager.getHaste(p)){
                     p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 15, 1, false));
                 }
-                b.setType(Material.BEDROCK);
-
            }
         }
     }
-
-    public void respawnOre(Material mat, Long dur, ItemStack b){
-
+    public void respawnOre(Material mat, Long dur, Block b){
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                b.setType(mat);
+            }
+        }.runTaskLater(TazPvP.getInstance(), dur);
     }
 }
 
