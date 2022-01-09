ca2745120b59c359894ae494c8f68e5cbcd8c2c0
rownox <rownox.ik@gmail.com>
2022-01-08 22:40:01 -0500

    Duel command start

--- src/main/java/net/ntdi/tazpvp/commands/playercommands/DuelCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/playercommands/DuelCommand.java
@@ -1,14 +1,27 @@
 package net.ntdi.tazpvp.commands.playercommands;
 
 import net.ntdi.tazpvp.TazPvP;
+import net.ntdi.tazpvp.commands.moderation.BanCommand;
 import net.ntdi.tazpvp.managers.DuelManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DuelCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        Player player = null;
+        if(sender instanceof Player) {
+            player = (Player) sender;
+        }
+
+        Player duel = Bukkit.getPlayer(args[0]);
         return true;
     }
 }
