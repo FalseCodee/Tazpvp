4b053088341af391dcf7d75181c7624f93886214
rownox <rownox.ik@gmail.com>
2022-01-08 19:44:09 -0500

    Added infinity arrow to rebirth

--- src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
@@ -1,9 +1,13 @@
 package net.ntdi.tazpvp.listeners.passive;
 
+import net.ntdi.tazpvp.TazPvP;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.HashMap;
 
@@ -22,6 +26,11 @@ public class BowListener implements Listener {
                     p.sendMessage(ChatColor.RED + "Slow down! "+ ChatColor.WHITE + secondsLeft +"s");
                     return;
                 }
+            }
+            ItemStack arrow = new ItemStack(Material.ARROW, 1);
+            PlayerInventory inv = p.getInventory();
+            if (TazPvP.statsManager.getRebirths(p) > 0) {
+                inv.addItem(arrow);
             }
             // No cooldown found or cooldown has expired, save new cooldown
             cooldowns.put(p.getName(), System.currentTimeMillis());
