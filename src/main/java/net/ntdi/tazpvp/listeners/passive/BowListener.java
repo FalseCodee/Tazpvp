d76b6b8edef01e07841e98d8de8702d5301e806c
rownox <rownox.ik@gmail.com>
2022-01-08 20:50:47 -0500

    Added inf arrow

--- src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
@@ -14,25 +14,24 @@ import java.util.HashMap;
 public class BowListener implements Listener {
     public HashMap<String, Long> cooldowns = new HashMap<String, Long>();
     int cooldownTime = 3;
-
+    ItemStack arrow = new ItemStack(Material.ARROW, 1);
     @EventHandler
     public void onShoot(org.bukkit.event.entity.EntityShootBowEvent event) {
         if (event.getEntity() instanceof Player) {
             Player p = (Player) event.getEntity();
-            if(cooldowns.containsKey(p.getName())) {
-                long secondsLeft = ((cooldowns.get(p.getName())/1000)+cooldownTime) - (System.currentTimeMillis()/1000);
-                if(secondsLeft>0) {
-                    event.setCancelled(true);
-                    p.sendMessage(ChatColor.RED + "Slow down! "+ ChatColor.WHITE + secondsLeft +"s");
-                    return;
-                }
-            }
-            ItemStack arrow = new ItemStack(Material.ARROW, 1);
             PlayerInventory inv = p.getInventory();
             if (TazPvP.statsManager.getRebirths(p) > 0) {
                 inv.addItem(arrow);
+                p.sendMessage("gh");
             }
-            // No cooldown found or cooldown has expired, save new cooldown
+            if(cooldowns.containsKey(p.getName())) {
+                long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
+                if (secondsLeft > 0) {
+                    event.setCancelled(true);
+                    p.sendMessage(ChatColor.RED + "Slow down! " + ChatColor.WHITE + secondsLeft + "s");
+                    return;
+                }
+            }
             cooldowns.put(p.getName(), System.currentTimeMillis());
         }
     }
