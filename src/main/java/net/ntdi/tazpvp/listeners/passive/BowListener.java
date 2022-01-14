e6c67577ef08e1aca4cff1f38de09f96c52e6a4b
professional-tdi <yogikngyt@gmail.com>
2022-01-13 20:25:58 -0500

    fixed bows

--- src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
@@ -17,41 +17,23 @@ public class BowListener implements Listener {
     public final HashMap<String, Long> cooldowns = new HashMap<>();
     final int cooldownTime = 3;
     final ItemStack arrow = new ItemStack(Material.ARROW, 1);
+
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        Player p = event.getPlayer();
-        PlayerInventory inv = p.getInventory();
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (p.getItemInHand().getType() == Material.BOW) {
-                if (cooldowns.containsKey(p.getName())) {
-                    long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
-                    if (secondsLeft > 0) {
-                        event.setCancelled(true);
-                        p.sendMessage(ChatColor.RED + "Slow down! ");
-                        return;
-                    } else if (TazPvP.statsManager.getRebirths(p) > 0) {
-                        inv.addItem(arrow);
-                    }
+    public void onShoot(org.bukkit.event.entity.EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player p = (Player) event.getEntity();
+            PlayerInventory inv = p.getInventory();
+            if (cooldowns.containsKey(p.getName())) {
+                long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
+                if (secondsLeft > 0) {
+                    event.setCancelled(true);
+                    p.sendMessage(ChatColor.RED + "Slow down! ");
+                    return;
+                } else if (TazPvP.statsManager.getRebirths(p) > 0) {
+                    inv.addItem(arrow);
                 }
-            }cooldowns.put(p.getName(), System.currentTimeMillis());
+            }
+            cooldowns.put(p.getName(), System.currentTimeMillis());
         }
     }
 }
-//    @EventHandler
-//    public void onShoot(org.bukkit.event.entity.EntityShootBowEvent event) {
-//        if (event.getEntity() instanceof Player) {
-//            Player p = (Player) event.getEntity();
-//            PlayerInventory inv = p.getInventory();
-//            if(cooldowns.containsKey(p.getName())) {
-//                long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
-//                if (secondsLeft > 0) {
-//                    event.setCancelled(true);
-//                    p.sendMessage(ChatColor.RED + "Slow down! ");
-//                    return;
-//                } else if (TazPvP.statsManager.getRebirths(p) > 0) {
-//                    inv.addItem(arrow);
-//                }
-//            }
-//            cooldowns.put(p.getName(), System.currentTimeMillis());
-//        }
-//    }
