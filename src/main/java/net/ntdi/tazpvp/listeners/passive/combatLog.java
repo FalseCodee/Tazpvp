333684ff2e7379eefafac3c318cd918305034dbf
professional-tdi <yogikngyt@gmail.com>
2022-01-26 16:22:05 -0500

    fixed combatlog warnrings

--- src/main/java/net/ntdi/tazpvp/listeners/passive/combatLog.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/combatLog.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -13,37 +14,22 @@ import java.util.List;
 public class combatLog implements Listener {
 
     public static final HashMap<Player, Integer> combatLog = new HashMap<>();
-    Integer dur = 15;
-
-    static List<Player> queue;
-
-    @EventHandler
-    public void onDamage(EntityDamageByEntityEvent e) {
-        if (e.getDamager() instanceof Player && e.getEntity() instanceof Player) {
-            Player p = (Player) e.getDamager();
-            Player killer = (Player) e.getEntity();
-//            combatLog.put(p, dur);
-//            combatLog.put(killer, dur);
-//            p.sendMessage(ChatColor.GREEN + "You have been added from combat log.");
-//            killer.sendMessage(ChatColor.GREEN + "You have been added from combat log.");
-        }
-    }
 
     public static void tick() {
+        List<Player> queue = new ArrayList<>();
+
         if (!combatLog.isEmpty()) {
             for (Player p : combatLog.keySet()) {
                 if (combatLog.get(p) - 1 > 0) {
                     combatLog.replace(p, combatLog.get(p) - 1);
                 } else {
-//                    queue.add(p);
-                    p.sendMessage(ChatColor.GREEN + "You have been removed from combat log.");
-                    combatLog.remove(p);
+                    queue.add(p);
                 }
             }
-//            for (Player p : queue) {
-//                p.sendMessage(ChatColor.GREEN + "You have been removed from combat log.");
-//                combatLog.remove(p);
-//            }
+            for (Player p : queue) {
+                combatLog.remove(p);
+                p.sendMessage(ChatColor.GREEN + "You have been removed from combat log.");
+            }
         }
     }
 }
