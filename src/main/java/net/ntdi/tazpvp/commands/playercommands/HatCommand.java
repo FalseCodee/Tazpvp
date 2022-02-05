cd9721637f7d5ead708dce66c60ad1646d9356ba
professional-tdi <yogikngyt@gmail.com>
2022-02-05 13:09:16 -0500

    Air for item in hand on ./hat

--- src/main/java/net/ntdi/tazpvp/commands/playercommands/HatCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/playercommands/HatCommand.java
@@ -1,6 +1,7 @@
 package net.ntdi.tazpvp.commands.playercommands;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,10 +14,7 @@ public class HatCommand implements CommandExecutor {
             Player p = (Player) sender;
             if (p.getItemInHand() != null) {
                 p.getInventory().setHelmet(p.getItemInHand());
-                p.getItemInHand().setAmount(0);
-                p.getItemInHand().setType(null);
-                p.getInventory().setHeldItemSlot(0);
-                p.getInventory().getItemInHand().setType(null);
+                p.getItemInHand().setType(Material.AIR);
                 p.sendMessage(ChatColor.GREEN + "You have equipped your item in your hand as your hat.");
             }
         }
