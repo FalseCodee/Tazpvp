6f65aaa77b9a8a14c26e1f35bb4eb7373409ff9a
professional-tdi <yogikngyt@gmail.com>
2022-02-05 16:54:20 -0500

    fixed unban offline

--- src/main/java/net/ntdi/tazpvp/commands/moderation/unbanCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/moderation/unbanCommand.java
@@ -3,6 +3,7 @@ package net.ntdi.tazpvp.commands.moderation;
 import net.ntdi.tazpvp.TazPvP;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,15 +20,24 @@ public class unbanCommand implements CommandExecutor {
             player = (Player) sender;
         }
 
-        Player banned = Bukkit.getPlayer(args[0]);
+        OfflinePlayer banned = Bukkit.getPlayer(args[0]);
         if (banned != null) {
             if(player != null && player.hasPermission("staff.ban")){
                 if(TazPvP.punishmentManager.isBanned(banned)){
                     TazPvP.punishmentManager.removeBan(banned);
                     player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN " + ChatColor.RED + "You just unbanned " + ChatColor.WHITE + banned.getName());
-                    banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
-                    banned.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN" + ChatColor.GRAY + " You've been unbanned by " + ChatColor.WHITE + player.getName());
-                    banned.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                    if (banned.isOnline()) {
+                        banned.getPlayer().sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                        banned.getPlayer().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "UNBAN" + ChatColor.GRAY + " You've been unbanned by " + ChatColor.WHITE + player.getName());
+                        banned.getPlayer().sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                        new BukkitRunnable() {
+
+                            @Override
+                            public void run() {
+                                TazPvP.getInstance().initScoreboard(banned.getPlayer());
+                            }
+                        }.runTaskLater(TazPvP.getInstance(), 20L);
+                    }
                     ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
                     String pexcmd = "pex user " + banned.getName() + " group remove banned";
                     Bukkit.dispatchCommand(console, pexcmd);
@@ -36,15 +46,6 @@ public class unbanCommand implements CommandExecutor {
                         BanCommand.bannedRunnables.remove(banned.getUniqueId());
                     }
 
-
-
-                    new BukkitRunnable() {
-
-                        @Override
-                        public void run() {
-                            TazPvP.getInstance().initScoreboard(banned);
-                        }
-                    }.runTaskLater(TazPvP.getInstance(), 20L);
 
                 } else {
                     player.sendMessage(ChatColor.GREEN + "You can't unban someone that's not banned!");
