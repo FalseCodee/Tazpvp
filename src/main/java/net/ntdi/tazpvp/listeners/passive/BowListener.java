524f2a328110e4f5fac9657f314c8c30c2076df9
rownox <rownox.ik@gmail.com>
2022-02-05 23:36:43 -0500

    No more bow cooldown in duels

--- src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/BowListener.java
@@ -23,14 +23,16 @@ public class BowListener implements Listener {
         if (event.getEntity() instanceof Player) {
             Player p = (Player) event.getEntity();
             PlayerInventory inv = p.getInventory();
-            if (cooldowns.containsKey(p.getName())) {
-                long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
-                if (secondsLeft > 0) {
-                    event.setCancelled(true);
-                    p.sendMessage(ChatColor.RED + "Slow down! ");
-                    return;
-                } else if (TazPvP.statsManager.getRebirths(p) > 0) {
-                    inv.addItem(arrow);
+            if (!p.getPlayer().getWorld().getName().equals("duel")) {
+                if (cooldowns.containsKey(p.getName())) {
+                    long secondsLeft = ((cooldowns.get(p.getName()) / 1000) + cooldownTime) - (System.currentTimeMillis() / 1000);
+                    if (secondsLeft > 0) {
+                        event.setCancelled(true);
+                        p.sendMessage(ChatColor.RED + "Slow down! ");
+                        return;
+                    } else if (TazPvP.statsManager.getRebirths(p) > 0) {
+                        inv.addItem(arrow);
+                    }
                 }
             }
             cooldowns.put(p.getName(), System.currentTimeMillis());
