b87f3cd191e3fb72d2e95121a521f9f4f5dfa95d
professional-tdi <yogikngyt@gmail.com>
2022-02-08 15:33:33 -0500

    made banned players unable to cast rod

--- src/main/java/net/ntdi/tazpvp/listeners/passive/RodListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/RodListener.java
@@ -3,6 +3,7 @@ package net.ntdi.tazpvp.listeners.passive;
 import net.ntdi.tazpvp.TazPvP;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
 
 public class RodListener implements Listener {
 
@@ -12,6 +13,13 @@ public class RodListener implements Listener {
             if (TazPvP.punishmentManager.isBanned(event.getPlayer())) {
                 event.setCancelled(true);
             }
+        }
+    }
+
+    @EventHandler
+    public void onRodShoot(PlayerFishEvent event) {
+        if (!event.getPlayer().hasPermission("op") && event.getPlayer().hasPermission("is.banned")) {
+            event.setCancelled(true);
         }
     }
 }
