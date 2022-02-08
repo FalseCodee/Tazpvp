0e8341915ab3952cdb4e28c8b525cf6858cf7d91
rownox <rownox.ik@gmail.com>
2022-02-07 19:56:24 -0500

    ger

--- src/main/java/net/ntdi/tazpvp/commands/playercommands/duelSpectateCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/playercommands/duelSpectateCommand.java
@@ -22,7 +22,6 @@ public class duelSpectateCommand implements CommandExecutor {
                     if (p.getPlayer().getWorld().getName().equals("spawn")) {
                         if (TazPvP.duelManager.isDueling(targetPlayer)) {
                             if (!DuelManager.spectating.contains(p)) {
-                                p.setGameMode(GameMode.SPECTATOR);
                                 p.teleport(targetPlayer.getLocation());
                                 TazPvP.duelManager.startSpectating(p);
                             } else {
