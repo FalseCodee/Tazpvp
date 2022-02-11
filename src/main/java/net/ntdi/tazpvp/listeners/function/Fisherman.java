a67f5f8189b0747a37260302809f460d6a9436c3
rownox <rownox.ik@gmail.com>
2022-02-11 15:55:11 -0500

    Balanced mining rewards

--- src/main/java/net/ntdi/tazpvp/listeners/function/Fisherman.java
+++ src/main/java/net/ntdi/tazpvp/listeners/function/Fisherman.java
@@ -18,65 +18,53 @@ public class Fisherman {
         if (Objects.equals(inhand, new ItemStack(Material.COAL_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, (int) (amount * 0.5));
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 1.2);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 1.2);
         } else if (Objects.equals(inhand, new ItemStack(Material.IRON_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 1.5);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 1.5);
         } else if (Objects.equals(inhand, new ItemStack(Material.GOLD_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 3);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 2);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 2);
         } else if (Objects.equals(inhand, new ItemStack(Material.LAPIS_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 4);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 2.5);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 2.5);
         } else if (Objects.equals(inhand, new ItemStack(Material.DIAMOND_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 7);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 4);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 4);
         } else if (Objects.equals(inhand, new ItemStack(Material.EMERALD_ORE, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 8);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 4.5);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 4.5);
         } else if (Objects.equals(inhand, new ItemStack(Material.COAL, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 2);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 2);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 2);
         } else if (Objects.equals(inhand, new ItemStack(Material.IRON_INGOT, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 3);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 3);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 3);
         } else if (Objects.equals(inhand, new ItemStack(Material.GOLD_INGOT, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 4);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 4);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 4);
         } else if (Objects.equals(inhand, new ItemStack(Material.INK_SACK, amount, (short) 4))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 5);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 5);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 5);
         } else if (Objects.equals(inhand, new ItemStack(Material.DIAMOND, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 12);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 8);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 8);
         } else if (Objects.equals(inhand, new ItemStack(Material.EMERALD, amount))) {
             p.getInventory().setItemInHand(new ItemStack(Material.AIR));
             TazPvP.statsManager.addMoney(p, amount * 15);
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Thank you for your business!");
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Here, take " + ChatColor.GOLD + "$" + amount * 9);
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " Great doing business!" + ChatColor.GOLD + "+ $" + amount * 9);
         } else {
-            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " I will take any ores you have!");
+            p.sendMessage(ChatColor.YELLOW + "[NPC] Miner:" + ChatColor.WHITE + " I will buy your ores.");
         }
     }
 }
