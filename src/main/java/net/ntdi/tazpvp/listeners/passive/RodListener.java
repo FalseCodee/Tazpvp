067b5d738782410b607f262c200927fb119f2edd
rownox <rownox.ik@gmail.com>
2022-02-21 07:55:28 -0500

    Fixed rodding while banned

--- src/main/java/net/ntdi/tazpvp/listeners/passive/RodListener.java
+++ src/main/java/net/ntdi/tazpvp/listeners/passive/RodListener.java
@@ -1,25 +1,19 @@
 package net.ntdi.tazpvp.listeners.passive;
 
 import net.ntdi.tazpvp.TazPvP;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class RodListener implements Listener {
-
-    @EventHandler
-    public void onPlayerFish(org.bukkit.event.player.PlayerFishEvent event) {
-        if (!event.getPlayer().hasPermission("op")) {
-            if (TazPvP.punishmentManager.isBanned(event.getPlayer())) {
-                event.setCancelled(true);
-            }
-        }
-    }
-
     @EventHandler
     public void onRodShoot(PlayerFishEvent event) {
         if (!event.getPlayer().hasPermission("op") && event.getPlayer().hasPermission("is.banned")) {
-            event.setCancelled(true);
+            Player p = event.getPlayer();
+            ItemStack item = p.getItemInHand();
+            p.getInventory().remove(item);
         }
     }
 }
