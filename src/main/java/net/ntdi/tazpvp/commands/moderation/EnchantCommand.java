8871393d784a6813c5543b93aa66df8c46e3a5c7
rownox <rownox.ik@gmail.com>
2022-03-04 18:58:51 -0500

    clean

--- src/main/java/net/ntdi/tazpvp/commands/moderation/EnchantCommand.java
+++ src/main/java/net/ntdi/tazpvp/commands/moderation/EnchantCommand.java
@@ -12,24 +12,18 @@ public class EnchantCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-
         if(player != null && player.hasPermission("staff.enchant") && args.length > 0 && args.length < 3) {
-            //args[]0
             Enchantment ench = Enchantment.getByName(args[0].toUpperCase());
             if (ench != null) {
                 PlayerInventory inv = player.getInventory();
                 if (inv.getItemInHand() != null) {
-
                     try{
                         Integer.parseInt(args[1]);
                         inv.getItemInHand().addUnsafeEnchantment(ench, Integer.parseInt(args[1]));
-
                     }catch(NumberFormatException e){
-
                         player.sendMessage(ChatColor.RED + "Your second arg should be a integer");
                         return false;
                     }
-
                 } else {
                     player.sendMessage(ChatColor.RED + "Hold something other than air!");
                     return false;
@@ -38,9 +32,7 @@ public class EnchantCommand implements CommandExecutor {
                 player.sendMessage(ChatColor.RED + "Enchant not found!");
                 return false;
             }
-
         }
         return true;
     }
-
 }
